6552e3c0a8829931687d193e82c7a4ca33696b1c
sarthak1366 <sarthak15k@outlook.com>
2023-08-23 01:43:55 +0530

    updated file

---
+++ 4 Introducing-numpy.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Its a single dimensional numpy array
+list1 = [1,2,3,4]
+print(np.shape(list1))
+
+#2D numpy array
+list2 = [[1,2,3] , [2,4,5]]
+print(np.shape(list2))
+
+#3D numpy array
+list3 = [[[1,2,3] , [2,4,5]] , [[1,5,3] , [25,4,5]]]
+print(np.shape(list3))
